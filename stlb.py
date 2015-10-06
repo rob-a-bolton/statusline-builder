00167e39cabe2664e1251d0be8815ff24dde7810
rab <badger.ea@gmail.com>
2015-10-06 12:21:28 +0100

    Added ability to put display text files as well as run scripts.

--- stlb.py
+++ stlb.py
@@ -22,6 +22,12 @@ class Script:
         self.last_line = self.out.readline().decode( 'utf-8' ).rstrip()
         print_all()
 
+class DisplayFile:
+    def __init__( self, path ):
+        self.path = path
+        self.name = Path( path ).name
+        self.last_line = Path( path ).read_text().replace("\n", " ")
+
 
 program_description="Runs a collection of scripts, printing their collective last line of output to stdout."
 program_name="statusline-builder"
@@ -40,10 +46,10 @@ args = vars(argparser.parse_args())
 script_dir = Path(args['script_dir'])
 async_loop = asyncio.get_event_loop()
 
-scripts = []
+outputs = []
 
 def print_all():
-    print( ' '.join( [script.last_line for script in scripts] ) )
+    print( ' '.join( [output.last_line for output in outputs] ) )
     sys.stdout.flush()
 
 # Properly exit upon catching signals
@@ -52,7 +58,7 @@ def sig_handler( signum, frame ):
 
 # Kill all our children, leaving no orphans
 def exit(code = 0):
-    for script in scripts:
+    for script in [output for output in outputs if isinstance( output, Script ) ]:
         script.process.terminate()
     sys.exit(code)
 
@@ -71,14 +77,22 @@ if not (script_dir.exists() and script_dir.is_dir()):
 for sig in [ signal.SIGILL, signal.SIGINT, signal.SIGTERM ]:
     signal.signal(sig, sig_handler)
 
-for script_file in [ file for file in script_dir.iterdir() if file.is_file() and os.access(str(file.resolve()), R_OK | X_OK)]:
-    path = str( script_file.resolve() )
-    script = Script( path )
-    scripts.append( script )
-    async_loop.add_reader( script.out, script.update )
+for file in [ file for file in script_dir.iterdir() if file.is_file() and os.access(str(file.resolve()), R_OK)]:
+    path = str( file.resolve() )
+    if os.access(path, X_OK):
+        script = Script( path )
+        outputs.append( script )
+        async_loop.add_reader( script.out, script.update )
+    else:
+        output = DisplayFile( path )
+        outputs.append( output )
 
-if not scripts:
-    exit_with_error("No scripts in %s" % script_dir)
+
+
+if not outputs:
+    exit_with_error("No scripts or outputs in %s" % script_dir)
+
+print_all()
 
 try:
     async_loop.run_forever()
